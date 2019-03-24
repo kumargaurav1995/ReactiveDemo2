160fabd71842282263bfb6ad95f2ca06d559938e
kumar gaurav <39881548+kumargaurav1995@users.noreply.github.com>
2019-03-25 02:33:30 +0530

    new1

--- demo2/src/main/java/com/reactive/demo2/Demo2Application.java
+++ demo2/src/main/java/com/reactive/demo2/Demo2Application.java
@@ -11,8 +11,9 @@ import java.util.concurrent.TimeUnit;
 public class Demo2Application {
 
 	public static void main(String[] args) {
-		userInput().flatMapMaybe(x -> Fibonacci.fibs().elementAt(x))
-				.blockingSubscribe(line -> System.out.println(line));
+		userInput().flatMapMaybe(x -> Fibonacci.fibs().elementAt(x))   //.filter(x -> x>500)
+				.blockingSubscribe(line -> System.out.println(line)
+						,System.out :: println ,() -> System.out.println("DONE"));
 
 
 		//SpringApplication.run(ReactiveJavaApplication.class, args);
@@ -20,12 +21,11 @@ public class Demo2Application {
 	public static Observable<Integer> userInput(){
 		Random random = new Random();
 		return Observable.intervalRange(0,20, 500, 500, TimeUnit.MILLISECONDS)
-				.concatMap(number -> Observable.just(random.nextInt(20))
-				.delay(random.nextInt(500), TimeUnit.MILLISECONDS));
-					//subscriber.onError(new RuntimeException("Wow Exception"));
+				.concatMap(number -> Observable.just(random.nextInt(20)));
+
+
 				}
 			}
-
 
 
 
